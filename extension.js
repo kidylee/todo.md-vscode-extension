99ccccac8214c26d69481500c3a452cbfd57eecf
Kidy Lee <kidylee@gmail.com>
2017-03-18 00:02:46 -0400

    read file

--- extension.js
+++ extension.js
@@ -1,7 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
-const Git = require('simple-git')(vscode.workspace.rootPath)
+
+const fs = require('fs')
+const path = require('path')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,18 +13,37 @@ function activate(context) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "todo-md" is now active!');
 
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     var disposable = vscode.commands.registerCommand('extension.gitCommitComment', function () {
         // The code you place here will be executed every time your command is executed
-        Git.show(['HEAD:TODO.MD'], (err, result) => {
-            var editor = vscode.window.activeTextEditor;
-            editor.edit(function (editBuilder) {
-                 editBuilder.insert(editor.selection.start, result);
+
+        let rootPath = vscode.workspace.rootPath
+
+        fs.readFile(path.join(rootPath, "TODO.MD"), (err, data) => {
+            if (err) {
+                vscode.window.showErrorMessage("Can't find TODO.MD in your workspace.")
+                return;
+            }
+
+            const Git = require('simple-git')(vscode.workspace.rootPath)
+            Git.show(['HEAD:TODO.MD'], (err, result) => {
+                if (err) {
+                    vscode.window.showErrorMessage(err)
+                    return;
+                }
+                var editor = vscode.window.activeTextEditor;
+                editor.edit(function (editBuilder) {
+                    editBuilder.insert(editor.selection.start, result);
+                })
+
             })
 
-        })
+        });
+
+
         // Display a message box to the user
 
     });

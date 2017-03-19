82d5c2ef5f2c7691d05aaa97196c2ed4071d0d4b
Kidy Lee <kidylee@gmail.com>
2017-03-18 22:04:42 -0400

    Release 1

--- extension.js
+++ extension.js
@@ -1,9 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
-
+var vscode = require('vscode')
 const fs = require('fs')
 const path = require('path')
+const ncp = require('copy-paste')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,28 +20,89 @@ function activate(context) {
     var disposable = vscode.commands.registerCommand('extension.gitCommitComment', function () {
         // The code you place here will be executed every time your command is executed
 
+
+
+        function parseDone(content) {
+            var done = /(?=\s*)[-\*] {0,2}\[x\].*$/gmi;
+            return content.match(done)
+        }
         let rootPath = vscode.workspace.rootPath
 
-        fs.readFile(path.join(rootPath, "TODO.MD"), (err, data) => {
+        if (!rootPath) {
+            var editor = vscode.window.activeTextEditor;
+            if (editor) {
+                let doc = editor.document;
+                let array = doc.fileName.match(/^.*?(?=\.git)/)
+                if (array && array.length == 1) {
+                    rootPath = array[0];
+                } else {
+                    vscode.window.showErrorMessage("Can't find git repository.");
+                    return;
+                }
+
+            } else {
+                vscode.window.showInformationMessage("Can't find ether TODO.MD or git repository.");
+                return;
+            }
+        }
+
+
+        fs.readFile(path.join(rootPath, "TODO.MD"), "utf-8", (err, data) => {
             if (err) {
                 vscode.window.showErrorMessage("Can't find TODO.MD in your workspace.")
                 return;
             }
 
-            const Git = require('simple-git')(vscode.workspace.rootPath)
+            var newDoneList = parseDone(data)
+
+            const Git = require('simple-git')(rootPath)
             Git.show(['HEAD:TODO.MD'], (err, result) => {
                 if (err) {
                     vscode.window.showErrorMessage(err)
                     return;
                 }
+                var oldDoneList = parseDone(result)
+
+                if (newDoneList.length > 0) {
+                    var set = new Set(newDoneList)
+
+                    if (oldDoneList) {
+                        oldDoneList.forEach((done) => {
+                            set.delete(done)
+                        })
+                    }
+
+                }
+
+                if (set, set.size == 0) {
+                    vscode.window.showInformationMessage("No finished Todo.")
+                    return
+                }
                 var editor = vscode.window.activeTextEditor;
-                editor.edit(function (editBuilder) {
-                    editBuilder.insert(editor.selection.start, result);
-                })
+                if (editor) {
+                    editor.edit(function (editBuilder) {
+                        set.forEach((done) => {
+                            editBuilder.insert(editor.selection.start, done + "\n");
+
+                        })
+                    })
+                } else {
+                    var result = "";
+                    for (let done of set) {
+                        result = result + done + "\n"
+                    }
+
+                    ncp.copy(result, () => {
+                        vscode.window.showInformationMessage("Copyed " + set.size + " item into clipboard.")
+                    })
+                }
+
 
             })
 
         });
+
+
 
 
         // Display a message box to the user
